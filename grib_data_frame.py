028f51d0fe40d992762ca5ab8a18d12e0805fa6a
Dustin Adams <dustinadams01@gmail.com>
2021-03-09 21:09:44 -0800

    Lat, lon, surf pres, and surf temp now extracted from grib.

--- grib_data_frame.py
+++ grib_data_frame.py
@@ -1,10 +1,17 @@
+# usage: $ python grib_data_frame.py <grib-file-path>
 import numpy as np
 from IPython import embed as shell
 import pygrib
 import sys
 
 f = pygrib.open(sys.argv[1])
-shell()
 
 all_gribs = [a for a in f]
 
+lats = np.array(all_gribs[0].latlons()[0])[0]
+lons = np.array(all_gribs[0].latlons()[1])[0]
+
+surf_pres = all_gribs[401].values.reshape(1, all_gribs[401].values.shape[0]*all_gribs[401].values.shape[1])[0]
+surf_temp = all_gribs[403].values.reshape(1, all_gribs[401].values.shape[0]*all_gribs[401].values.shape[1])[0]
+
+shell()
