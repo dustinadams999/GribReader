06f9a0af4347cf30952e955dbe93899896164248
Dustin Adams <dustinadams01@gmail.com>
2021-03-12 00:38:45 +0000

    Adding binary list of whether gps point is land or not for use with logistic regression.

--- grib_data_frame.py
+++ grib_data_frame.py
@@ -1,6 +1,7 @@
 # usage: $ python grib_data_frame.py <grib-file-path>
-import numpy as np
+from global_land_mask import globe
 from IPython import embed as shell
+import numpy as np
 import pygrib
 import sklearn
 import sys
@@ -17,5 +18,11 @@ lons = np.array(all_gribs[0].latlons()[1].reshape(outer*inner))
 
 surf_pres = np.array(all_gribs[401].values.reshape(1, outer*inner)[0])
 surf_temp = np.array(all_gribs[403].values.reshape(1, outer*inner)[0])
+
+is_land = []
+for i in range(len(lats)):
+    is_land.append(globe.is_land(lats[i],lons[i]-180))
+
+is_land = np.array(is_land)
 
 shell()
